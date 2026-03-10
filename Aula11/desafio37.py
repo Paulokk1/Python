79365b75127c6c752bd1616c6b988010ad4a7ac3
Paulo Filipe <felipepauloxd560@gmail.com>
2026-03-10 19:14:47 -0300

    Update desafio37.py

--- Aula11/desafio37.py
+++ Aula11/desafio37.py
@@ -4,10 +4,10 @@ print('[ 2 ] converter para Octal')
 print('[ 3 ] converter para Hexadecimal')
 res = int(input('Qual o valor escolhido: '))
 if res == 1:
-    print('O valor em binário é {}'.format(bin(numero)))
+    print('O valor em binário é {}'.format(bin(numero)[2:]))
 elif res == 2:
-    print('O valor em octal é {}'.format(oct(numero)))
+    print('O valor em octal é {}'.format(oct(numero)[2:]))
 elif res == 3:
-    print('O valor hexadecimal é {}'.format(hex(numero))) 
+    print('O valor hexadecimal é {}'.format(hex(numero)[2:])) 
 else:
     print('Opção invalida')
